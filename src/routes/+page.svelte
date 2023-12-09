6fa9ff8d879837fec0a9b6f05934039569924340
teznote <teznote@outlook.com>
2023-12-09 14:55:53 +0900

    f

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="msg"></div>
+
+<script>
+  const a = document.querySelector('#msg')
+  a.innerHTML = 'hi'
+</script>
