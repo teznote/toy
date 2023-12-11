e6513dcd078124df8a57ca01e25a4799aa3e5495
null <null>
2023-12-11 21:33:26 +0900

    ff

--- src/build.js
+++ src/build.js
@@ -9,9 +9,9 @@ import cssnano from 'cssnano'
 import { Liquid } from 'liquidjs'
 import { minify } from 'html-minifier'
 import hljs from 'highlight.js'
-import { Marked } from 'marked'
-import { markedHighlight } from 'marked-highlight'
+import markdownIt from 'markdown-it'
 
+// 
 
 // init liquidjs
 const engine = new Liquid()
@@ -43,45 +43,42 @@ hljs.registerLanguage("pseudo", function(hljs) {
 })
 
 // md renderer init and customize for line highlighting
-const marked = new Marked(
-  markedHighlight({
-    langPrefix: 'hljs language-',
-    highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+const parse_md = markdownIt({
+  html: true,
+  xhtmlOut: true,
+  highlight(code, lang) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext'
 
-      let lines = code.trim().split('\n')
-      let tar_line = new Map()
-      let code_modified = lines.map((x, i) => {
-        if (x[0] === '-' || x[0] === '+' || x[0] === '~') {
-          tar_line.set(i, x[0])
-          x = x.slice(1)
-        }
-        return x
-      }).join('\n')
+    let lines = code.trim().split('\n')
+    let tar_line = new Map()
+    let code_modified = lines.map((x, i) => {
+      if (x[0] === '-' || x[0] === '+' || x[0] === '~') {
+        tar_line.set(i, x[0])
+        x = x.slice(1)
+      }
+      return x
+    }).join('\n')
 
-      lines = hljs.highlight(code_modified, { language }).value.trim().split('\n')
-      return lines.map((x, i) => {
-        return `<div class="line ${tar_line.get(i) || ''}">${x || ' '}</div>`
-      }).join('')
-    },
+    lines = hljs.highlight(code_modified, { language }).value.trim().split('\n')
+    return lines.map((x, i) => {
+      return `<div class="line ${tar_line.get(i) || ''}">${x || ' '}</div>`
+    }).join('')
+  },
+})
+
+// md file -> json sting convert recursive function
+function convert(file, content='', props={}) {
+  props.layout = null
+  const r = matter.read(file, {
+    engines: { yaml: s => yaml.safeLoad(s, { schema: yaml.JSON_SCHEMA }) }
   })
-)
-
-// md file -> json sting convert function
-function convert(file) {
-  (function fn(file, content, props) {
-    const r = matter.read(file, {
-      engines: { yaml: s => yaml.safeLoad(s, { schema: yaml.JSON_SCHEMA }) }
-    })
-    if (file.match(/\.md$/)) {
-      content = marked.parse(r.content)
-      let page = Object.assign(page, r.data)
-      return ('layout' in r.data) ? fn(`./_layouts/${r.data.layout}.html`, content, page) : { content, page }
-    } else {
-      content = engine.parseAndRenderSync(r.content, { content, page, ...r.data })
-      return ('layout' in r.data) ? fn(`./_layouts/${r.data.layout}.html`, content, page) : { content, page }
-    }
-  })(file, '', {})
+  Object(props, r.data)
+  if (file.match(/\.md$/)) {
+    content = parse_md.render(r.content)
+  } else {
+    content = engine.parseAndRenderSync(r.content, { content, ...props })
+  }
+  return (r.data?.layout) ? convert(`./_layouts/${r.data.layout}.html`, content, ...props) : { content, ...props } 
 }
 
 // build _pages
@@ -90,10 +87,8 @@ function build_pages() {
   const mdinfos = []
 
   for (let mdfile of mdfiles) {
+    const { content, title } = convert(mdfile)
     const pathname = mdfile.split('/_pages')[1].replace(/\[.*?\]/, '').replace(/\.md$/, '')
-    let { content, data } = matter.read(mdfile, {
-      engines: {yaml: s => yaml.safeLoad(s, { schema: yaml.JSON_SCHEMA })}
-    })
-    content = marked.parse(content)
+
   }
 }
