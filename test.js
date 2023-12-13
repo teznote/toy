a6c534b5f097c7c4182f3c25568793279e764c9e
null <null>
2023-12-13 11:04:37 +0900

    init work (회사업무)

--- test.js
+++
@@ -1,6 +0,0 @@
-import yaml from 'js-yaml'
-import fs from 'fs-extra'
-
-const menu = yaml.load(fs.readFileSync('./src/_pages/navmenu.yaml', 'utf8'))
-console.log(menu)
-console.log(Object.keys(menu))
