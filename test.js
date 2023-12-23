ef2c25b16bd1d954c48e21e2e1590aed4324ab84
null <null>
2023-12-23 21:45:31 +0900

    fff

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 import render from './md_renderer.js'
+import fg from 'fast-glob'
+import fs from 'fs-extra'
+import path from 'node:path'
+const $root = process.env.PWD
 
-const a = render('./src/_pages/[]index.md')
 
-console.log(a)
+const mdinfos = fg.globSync($root + '/src/_pages/**/*.md')
+for (let x of mdinfos) {
+  console.log(path.parse(x))
+}
